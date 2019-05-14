34d9f2b59743b0baf4252da67aa04429b666b0f7
Martin Haferanke <martin.haferanke@th-brandenburg.de>
2019-05-14 15:12:11 +0200

    Adding Component Product List and Product Interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Album} from './album';
+import {Product} from './product';
 import 'rxjs/add/operator/map';
 import {Http, Response} from '@angular/http';
+
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productUrl = '../assets/products.json';
   constructor(private _http: HttpClient) { }
 
   getAlbum(id: number): Observable<Album> {
    return this._http.get<Album>(this._albumUrl);
+  }
+
+  getProducts(): Observable<Product> {
+    return this._http.get<Product>(this._productUrl);
   }
 }
