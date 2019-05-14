68f1b5405cb82808f255d1eb18a8a20bb8619b19
Martin Haferanke <martin.haferanke@th-brandenburg.de>
2019-05-14 13:29:45 +0200

    adding Image URL dynamicaly

---
+++ src/app/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+  constructor(private _http: HttpClient) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl);
+  }
+}
