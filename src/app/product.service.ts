7432629a55ed42b3646ffdc246842e4921103610
Martin Haferanke <martin.haferanke@th-brandenburg.de>
2019-05-14 14:30:51 +0200

    Adding Track and Album Interface Types to the data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {Album} from './album';
+import 'rxjs/add/operator/map';
+import {Http, Response} from '@angular/http';
 
 @Injectable()
 export class ProductService {
@@ -7,7 +11,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: HttpClient) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl);
+  getAlbum(id: number): Observable<Album> {
+   return this_http.get(this._albumUrl.map(response => response.json()));
+    //return this._http.get<Album>(this._albumUrl);
   }
 }
